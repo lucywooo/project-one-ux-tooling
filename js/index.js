90841c17e7ba8ed61db8af5d8e8fdd63d073c5e7
luciwoo <yiting.wu@georgebrown.ca>
2020-03-06 16:08:31 -0500

    Experimented with window and doc

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 
+let bar = document.querySelector('.progress')
+
+window.addEventListener('scroll',event => {
+    // Window Height
+    let windowH = window.innerHeight
+    // Document Height
+    let documentH = document.documentElement.scrollHeight
+    //How much has been scrolled
+    let amntScrolled = window.scrollY
+    //How much Can be scrolled
+    let ttlAvailable = documentH - windowH
+    //What percentage of the scrollable is scrolled
+    let percent = amntScrolled/ttlAvailable
+
+    //Take the decimal percentage and convert to
+    bar.style.width = `{percent * 100}%`
+    bar.textContent = `${Math.round(percent*100)}%`
+})
+
+window.addEventListener('mousemove',event=> {
+    console.log(event.clientX, event.CLIENT)
+})
