809e89da814d50364a2adba018daa8236665e4f3
lucywooo <lucywuyiting@hotmail.com>
2020-03-29 02:01:01 -0400

    new

--- js/index.js
+++ js/index.js
@@ -1,27 +1,3 @@
-// let $bar = document.querySelector('.progress')
-// let $progressText = document.querySelector('.progressText')
-
-// window.addEventListener('scroll', event => {
-// let windowH = window.innerHeight
-// let documentH = document.documentElement.scrollHeight
-// let amtScrolled = window.scrollY
-// let ttlAvailable = documentH - windowH
-// let percent = amtScrolled / ttlAvailable
-// $bar.style.width = `${percent * 100}%`
-// $progressText.textContent = `${Math.round(percent*100)}%`
-// let h1Top = $h1.offsetTop
-// let h1Height = $h1.clientHeight
-// console.log(h1Top, h1Height, amtScrolled)
-// if (amtScrolled > h1Top + h1Height) {
-//     console.log('H1 is off the top')
-//     $titleBar.classList.add(`show`)
-//     $titleBar.innerHTML = `${title}`;
-// } else {
-//     $titleBar.classList.remove(`show`)
-//     $titleBar.innerHTML = ``;
-// }
-// })
-
 // sticky menu
 window.onscroll = function() {myFunction()};
 
